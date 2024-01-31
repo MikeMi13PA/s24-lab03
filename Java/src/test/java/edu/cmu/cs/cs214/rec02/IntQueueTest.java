015d58571dd1059ddc24b814097aa433672b4ead
Ke Mi <kemi@andrew.cmu.edu>
2024-01-31 13:14:55 -0500

    Write specification tests for LinkedIntQueue

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -52,20 +52,20 @@ public class IntQueueTest {
 
     @Test
     public void testNotEmpty() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(testList.get(0));
+        assertTrue(!mQueue.isEmpty());
     }
 
     @Test
     public void testPeekEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        assertEquals(null, mQueue.peek());
     }
 
     @Test
     public void testPeekNoEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(testList.get(0));
+        mQueue.enqueue(testList.get(1));
+        assertEquals(testList.get(0), mQueue.peek());
     }
 
     @Test
@@ -80,8 +80,13 @@ public class IntQueueTest {
 
     @Test
     public void testDequeue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        for (int i = 0; i < testList.size(); i++) {
+            mQueue.enqueue(testList.get(i));
+        }
+        for (int i = 0; i < testList.size(); i++) {
+            assertEquals(testList.get(i), mQueue.dequeue());
+            assertEquals(testList.size() - (i + 1), mQueue.size());
+        }
     }
 
     @Test
